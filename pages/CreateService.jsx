d4663157843f067f71fb147a0940219c11c47088
dudalszz <duclsza@gmail.com>
2025-10-05 12:20:41 -0300

    tentando subir

--- pages/CreateService.jsx
+++
@@ -1,5 +0,0 @@
-// criando um serviço quem vai chamar vai ser um usuario vai estar cadastrando um serviço
-
-// criar um serviço contratando um profissional diretamente
-
-//  serviço gerado -> profissional aceita ?
