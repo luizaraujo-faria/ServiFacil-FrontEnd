7548ab578c8ceaa3418d7ae8bba5722aae66c90d
luiz <luiz308araujofarias@gmail.com>
2025-11-11 19:11:44 -0300

    config: Configurando Nova Rota da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8081/api';
+const API_BASE_URL = ' https://servifacil-backend.onrender.com/api';
 
 // Instância do axios com configuração base
 const api = axios.create({
